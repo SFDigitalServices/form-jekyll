32cc18184b37a649eb18e60c62dfcc7d094d0062
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2019-07-29 14:01:04 -0700

    Add pagination and settings

--- js/script.js
+++ js/script.js
@@ -1,13 +1,46 @@
 $(document).ready(function(){
+
+  // Initial state
+  $('.form-section:first-child').addClass('active');
   $(".form").validator();
 
-  $("input[data-shows]").change(function () {
+  // Pagination
+  $('.form-section-prev').click(function() {
+    var currentSection = $(this).parent().parent('.form-section');
+    currentSection.removeClass('active');
+    currentSection.prev('.form-section').addClass('active');
+    e.preventDefault();
+  });
+
+  $('.form-section-next').click(function() {
+    var currentSection = $(this).parent().parent('.form-section');
+    currentSection.removeClass('active');
+    currentSection.next('.form-section').addClass('active');
+    e.preventDefault();
+  });
+
+  // Conditionals
+  $("input[type='radio'][data-shows]").change(function () {
     var group = $(this).attr('data-shows');
     $("div").find("[data-group='" + group + "']").show();
+  });
+
+  $("input[type='checkbox'][data-shows]").change(function () {
+    var group = $(this).attr('data-shows');
+    $("div").find("[data-group='" + group + "']").toggle();
   });
 
   $("input[data-hides]").change(function () {
     var group = $(this).attr('data-hides');
     $("div").find("[data-group='" + group + "']").hide();
   });
+
+  // Settings
+  $('#settings-all-pages').change(function() {
+    $('body').toggleClass('all-pages');
+  })
+
+  $('#settings-all-conditionals').change(function() {
+    $('body').toggleClass('all-conditionals');
+  })
 });
